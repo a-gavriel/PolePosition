7abfe0fd083ce0210e8bd683c067e9c2a38467e4
Ema0898 <aguisnchez@gmail.com>
2018-06-17 16:29:19 -0600

    Agrega JavaDoc

--- src/game/Shoot.java
+++ src/game/Shoot.java
@@ -1,15 +1,38 @@
 package game;
 
-
-
+/**
+ * Clase encargada de los disparos de los jugadores
+ */
 public class Shoot extends GameObject
 {
+    /**
+     * Tamaño en X de la bala
+     */
     private static final int SIZEX = 2;
+    /**
+     * Tamaño en Y de la bala
+     */
     private static final int SIZEY = 2;
-    private static final float SPEED = 12f;
+    /**
+     * Velocidad de la bala
+     */
+    private static final float SPEED = 11f;
+    /**
+     * Tipo de la bala
+     */
     private String type;
+    /**
+     * Daño de la bala
+     */
     private int dmg;
-    
+
+    /**
+     * Incia los parametros
+     * @param x Posicion en X
+     * @param y Posicion en Y
+     * @param type Tipo de bala
+     * @param rot1 Rotacion
+     */
     public Shoot (float x , float y, String type, float rot1)
     {
         this.x = x;
@@ -21,9 +44,11 @@ public class Shoot extends GameObject
         this.dmg = 1;
         this.rot = rot1;
     }
-    
-    
-@Override
+
+    /**
+     * Actualiza los atributos de las balas
+     */
+    @Override
     public void update()
     {
 	if(type.equalsIgnoreCase("Heroe")){
@@ -36,8 +61,12 @@ public class Shoot extends GameObject
         x += SPEED;
         }
     }
+
+    /**
+     * Dibuja las balas
+     */
     @Override
-    public void render(){
+    public void render() {
         Draw.drawBullet(x,y,sx/2,sy/2, rot);
     }
     
