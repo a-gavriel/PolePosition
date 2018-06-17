cc4740f0713a9db6ecad0ca96970fced916ed67e
Ema0898 <aguisnchez@gmail.com>
2018-06-17 16:52:52 -0600

    Agrega JavaDoc

--- src/Interfaces/InitInterface2.java
+++ src/Interfaces/InitInterface2.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Interfaces;
 import game.Main;
 /**
  *
- * @author Crisptofer
+ * Clase encargada de mostrar los colores disponibles
  */
 
 public class InitInterface2 extends javax.swing.JFrame {
