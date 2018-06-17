cb4382bec1b50ae5e46ca0d6b0a6667e3e68d593
Ema0898 <aguisnchez@gmail.com>
2018-06-17 16:35:08 -0600

    Agrega JavaDoc

--- src/game/Draw.java
+++ src/game/Draw.java
@@ -3,15 +3,31 @@ package game;
 
 import static org.lwjgl.opengl.GL11.*;
 
-
-
+/**
+ * Clase encargada de dibujar los elementos del juego
+ */
 public class Draw {
-    
+
+    /**
+     * Dibuja un rectangulo
+     * @param x Posicion en X
+     * @param y Posicion en Y
+     * @param width Ancho
+     * @param height Alto
+     */
     public static void rect(float x, float y, float width, float height)
     {
         rect( x,  y,  width,  height, 0);
     }
-    
+
+    /**
+     * Dibuja un rectangulo
+     * @param x Posicion en X
+     * @param y Posicion en Y
+     * @param width Ancho
+     * @param height Alto
+     * @param rot Rotacion del rectangulo
+     */
     public static void rect(float x, float y, float width, float height, float rot)
     {
         glPushMatrix();
@@ -29,15 +45,28 @@ public class Draw {
             glEnd();
         }
         glPopMatrix();
-    }     
-    
-    
+    }
+
+
+    /**
+     * Dibuja un rectangulo
+     * @param x Posicion en X
+     * @param y Posicion en Y
+     * @param width Ancho
+     * @param height Alto
+     * @param red Si es rojo
+     * @param green Si es verde
+     * @param blue Si es azul
+     */
     public static void rect(float x, float y, float width, float height, float red,float green,float blue)
     {
         rect( x,  y,  width,  height, 0, red,green,blue);
         
     }
-    
+
+    /**
+     * Dibuja el Césped
+     */
     public static void drawGrass(){
         
     }
@@ -65,7 +94,17 @@ public class Draw {
         }
         glPopMatrix();
     }
-    
+
+    /**
+     * Dibuja un cubo
+     * @param x Posicion en X
+     * @param y Posicion en Y
+     * @param width Ancho
+     * @param height Alto
+     * @param red Si es rojo
+     * @param green Si es verde
+     * @param blue Si es azul
+     */
     public static void cube(float x, float y, float width, float height,float rot,float red,float green,float blue)
     {
         glPushMatrix();
@@ -145,6 +184,17 @@ public class Draw {
         }
         glPopMatrix();
     }
+
+    /**
+     * Dibuja una bala
+     * @param x Posicion en X
+     * @param y Posicion en Y
+     * @param width Ancho
+     * @param height Alto
+     * @param red Si es rojo
+     * @param green Si es verde
+     * @param blue Si es azul
+     */
     public static void drawBullet(float x, float y, float width, float height,float rot){
         glPushMatrix();
         {
@@ -153,76 +203,75 @@ public class Draw {
             glRotatef(rot,0,0,1);
             glBegin(GL_QUADS);
             /*{
-                
-                glColor3f(0,0,1);                
+
+                glColor3f(0,0,1);
                 glVertex3f(0,0,1);
                 glVertex3f(0,height,1);
                 glVertex3f(width,height,1);
-                glVertex3f(width,0,1);   
-                
+                glVertex3f(width,0,1);
+
                 glColor3f(1, 0,0);
                 glVertex3f(0,0,5);
                 glVertex3f(0,height,5);
                 glVertex3f(width,height,5);
-                glVertex3f(width,0,5);  
-                
+                glVertex3f(width,0,5);
+
             }
              */
             {
 //front face
-                    glColor3f(1, 1, 0);
+                    glColor3f(0, 0, 0);
                     glVertex3f(0, height, 2);
                     glVertex3f(0, height, 3);
                     glVertex3f(width,height, 3);
                     glVertex3f(width, height, 3);
-                    
+
 
 //BackFace
-                    glColor3f(1, 1, 0);
+                    glColor3f(0, 0, 0);
                     glVertex3f(0, 0, 2);
                     glVertex3f(0, 0, 2);
                     glVertex3f(width,0, 3);
                     glVertex3f(width, 0, 2);
-                   
+
 
 //BottomFace
-                    glColor3f(1,1,0);                
+                    glColor3f(0,0,0);
                     glVertex3f(0,0,2);
                     glVertex3f(0,height,2);
                     glVertex3f(width,height,2);
-                    glVertex3f(width,0,2);  
-                    
+                    glVertex3f(width,0,2);
+
 
 //TopFace
-                    glColor3f(1, 1,0);
+                    glColor3f(0, 0,0);
                     glVertex3f(0,0,3);
                     glVertex3f(0,height,3);
                     glVertex3f(width,height,3);
-                    glVertex3f(width,0,3); 
-                    
+                    glVertex3f(width,0,3);
+
 
 //LeftFace
-                    glColor3f(1, 1, 0);
-                    glVertex3f(0, 0, 2);                    
+                    glColor3f(0, 0, 0);
+                    glVertex3f(0, 0, 2);
                     glVertex3f(0, 0, 3);
                     glVertex3f(0,height, 3);
                     glVertex3f(0, height, 2);
-                    
+
 
 //Right Face
-                    glColor3f(1, 1, 0);
-                    glVertex3f(width, 0, 2);                    
-                    glVertex3f(width, 0, 3);                    
+                    glColor3f(0, 0, 0);
+                    glVertex3f(width, 0, 2);
+                    glVertex3f(width, 0, 3);
                     glVertex3f(width,height, 3);
                     glVertex3f(width, height, 2);
             }
-            
+
 
             glEnd();
         }
         glPopMatrix();
-        
+
     }
-    
     
 }
